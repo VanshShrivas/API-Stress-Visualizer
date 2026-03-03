34fd227f906a1ec45d7743f367a82f38982e5587
Vansh Shrivas <vansh.shrivas.2024@gmail.com>
2026-03-03 17:37:21 +0530

    Added testState format

---
+++ Backend/utils/createTestState.js
@@ -0,0 +1,18 @@
+//this creates a new testState for the user 
+
+export default function createTestState(config){
+    let testState= {
+        id:randomUUID(),
+        config,
+        status: "running",
+        totalRequests: config.totalRequests,
+        concurrency: config.concurrency,
+        completed: 0,
+        success: 0,
+        errors: 0,
+        latencies: [],
+        startTime: Date.now(),
+        endTime: null
+    }
+    return testState;
+}
