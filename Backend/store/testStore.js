f36a094c2a07cbeee67a6f6899b9b58bc49bcb06
Vansh Shrivas <vansh.shrivas.2024@gmail.com>
2026-03-03 17:36:13 +0530

    Created on-server memory map

---
+++ Backend/store/testStore.js
@@ -0,0 +1,11 @@
+const tests=new Map(); //on server memory to improve performance...
+
+export function getTestState(id){
+    return tests.get(id);
+}
+export function addTestState(testState){
+    tests.set(testState.id,testState);
+}
+export function deleteTestState(testState){
+    tests.delete(testState.id);
+}
